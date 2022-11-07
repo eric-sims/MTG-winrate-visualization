43e040c1d76f0bc457a9cbb027ad538cf4f0f355
eric-sims <e.sims@icloud.com>
2022-11-07 12:41:37 -0700

    Code skeletons and files added

--- js/script.js
+++ js/script.js
@@ -2,7 +2,16 @@
 Promise.all([d3.csv('../data/Utah_Crash_Data_2020.csv')]).then( data =>
     {
         let crashData = data[0];
-        
-        let table = new googleMap(crashData);
-        table.draw();
+
+        globalApplicationState.map = new googleMap(crashData);
+        globalApplicationState.map.draw();
     });
+
+
+const globalApplicationState = {
+    map: null,
+    data: null,
+    lineChartSeasonal: null,
+    lineChartHourly: null,
+    filterSection: null,
+};
