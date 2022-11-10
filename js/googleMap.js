a71af6d22332ef27a2a7a991320448cb679f9e9f
Alan Bird <alanbird1999@gmail.com>
2022-11-09 21:02:31 -0700

    removed console.log

--- js/googleMap.js
+++ js/googleMap.js
@@ -16,79 +16,59 @@ class googleMap {
         latLngBounds: { north: 43, east: -108, south: 36, west: -114 },
         strictBounds: false,
       },
-    }; 
+    };
 
     // Create the Google Map…
     let map = new google.maps.Map(d3.select("#map").node(), options);
 
     let overlay = new google.maps.OverlayView();
 
-    // console.log('overlay', overlay);
-
     let data = this.globalApplicationState.data.slice(0, 100);
     overlay.onAdd = function () {
-          console.log(this.getPanes());
-
-        let layer = d3
+      let layer = d3
         .select(this.getPanes().overlayMouseTarget)
         .append("div")
         .attr("class", "crashPoints");
 
-      console.log('layer', layer);
-
       overlay.onRemove = function () {
-          d3.select('.crashPoints').remove();
-        };
-      
-      console.log('eheheehehre')
+        d3.select(".crashPoints").remove();
+      };
+
       overlay.draw = function () {
+        let projection = this.getProjection();
 
-        let projection = this.getProjection(), padding = 10;
-
-        let marker = layer.selectAll("svg")
-          .data(data)
-          ;
-
-        console.log('marker', marker);
-
-        let markerEnter = marker.enter().append('svg');
-
-        console.log('markerEnter', markerEnter);
+        let marker = layer.selectAll("svg").data(data);
+        let markerEnter = marker.enter().append("svg");
 
         // add a circle
         markerEnter.append("circle");
 
         marker.exit().remove();
-        
+
         marker = marker.merge(markerEnter);
 
-        marker
-          .each(transform)
-          .attr("class", "marker")
-          ;
+        marker.each(transform).attr("class", "marker");
 
-        marker.select('circle')
+        marker
+          .select("circle")
           .attr("r", 5)
-          .attr('cx', padding+5)
-          .attr('cy', padding+5)
-          .attr('fill', 'red')
-          .on('click', function(d) {
+          .attr("cx", 0)
+          .attr("cy", 0)
+          .attr("fill", "red")
+          .on("click", function (d) {
             console.log(d);
           });
 
         function transform(d) {
           d = new google.maps.LatLng(+d.LAT_UTM_Y, +d.LONG_UTM_X);
           d = projection.fromLatLngToDivPixel(d);
-          return d3.select(this)
-              .style("left", (d.x - padding) + "px")
-              .style("top", (d.y - padding) + "px");
+          return d3
+            .select(this)
+            .style("left", d.x + "px")
+            .style("top", d.y + "px");
         }
-
       };
-      
-      console.log('after overlay.draw');
     };
     overlay.setMap(map);
   }
-
 }
