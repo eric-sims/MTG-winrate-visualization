79a87f32f7d914a50fe485e60d4ebf3ced003a19
eric-sims <e.sims@icloud.com>
2022-11-09 23:19:51 -0700

    got rid of some extra fluff code

--- js/googleMap.js
+++ js/googleMap.js
@@ -3,9 +3,7 @@ class googleMap {
     this.globalApplicationState = globalApplicationState;
   }
 
-  draw() {
-    console.log(this.globalApplicationState.data);
-
+  async draw() {
     let options = {
       zoom: 7,
       center: {
@@ -19,75 +17,6 @@ class googleMap {
       },
     };
 
-    // Create the Google Map…
-    let map = new google.maps.Map(d3.select("#map").node(), options);
-
-    let overlay = new google.maps.OverlayView();
-
-    let data = this.globalApplicationState.data.slice(0, 10);
-
-    overlay.onAdd = function () {
-      let layer = d3
-        .select(this.getPanes().overlayMouseTarget).append("div")
-        .attr("class", "crashPoints");
-
-      overlay.onRemove = function () {
-        d3.select(".crashPoints").remove();
-      };
-
-      overlay.draw = function () {
-        let projection = this.getProjection(),
-          padding = 10;
-
-        let marker = layer.selectAll("svg").data(data);
-        let markerEnter = marker.enter().append("svg");
-
-        // add a circle
-        markerEnter.append("circle");
-
-        marker.exit().remove();
-
-        marker = marker.merge(markerEnter);
-
-        marker
-          .each(transform)
-          .attr("class", "marker");
-
-        marker.select("circle")
-          .attr("r", 5)
-          .attr("cx", padding)
-          .attr("cy", padding)
-          .attr("fill", "red")
-          .on("click", d => console.log(d));
-
-        function transform(d) {
-          d = new google.maps.LatLng(+d.lat, +d.lon);
-          d = projection.fromLatLngToDivPixel(d);
-          return d3
-            .select(this)
-            .style("left", d.x - padding + "px")
-            .style("top", d.y - padding + "px");
-        }
-      };
-    };
-
-    overlay.setMap(map);
-  }
-
-  async drawGoogleMap() {
-    let options = {
-      zoom: 7,
-      center: {
-        lat: 39.321,
-        lng: -111.0937,
-      },
-      mapTypeId: "roadmap",
-      restriction: {
-        latLngBounds: { north: 43, east: -108, south: 36, west: -114 },
-        strictBounds: false,
-      },
-    };
-    
     // Create the Google Map…
     let map = new google.maps.Map(d3.select("#map").node(), options);
 
