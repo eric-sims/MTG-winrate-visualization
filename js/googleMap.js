e9fd4c64a9eece3385baa009e54f032d9e98607b
eric-sims <e.sims@icloud.com>
2022-11-30 21:06:17 -0700

    google maps stuff

--- js/googleMap.js
+++ js/googleMap.js
@@ -15,99 +15,116 @@ class googleMap {
 
     this.map = new google.maps.Map(d3.select("#map").node(), options);
 
-    this.overlay = new google.maps.OverlayView();
+    // this.overlay = new google.maps.OverlayView();
 
     this.updateCircles();
   }
 
   async updateCircles() {
     let data = this.globalApplicationState.filteredData.slice(0, 1000);
-    let dataCount = data.length; // We should display a string saying how many crashes we are showing
-    let overlay = this.overlay;
 
-    //Create the overlay that we will draw on
+    // add a google maps marker for every data point
+    const markers = data.map((d) => {
+      return new google.maps.Marker({
+        position: {
+          lat: +d.lat,
+          lng: +d.lon,
+        },
+        label: {
+          text: d.CRASH_ID,
+        },
+      });
+    });
 
-    // Add the container when the overlay is added to the map.
-    overlay.onAdd = function () {
-      let layer = d3
-        .select(this.getPanes().overlayMouseTarget)
-        .append("div")
-        .attr("class", "accident");
+    // Add a marker clusterer to manage the markers.
+    new markerClusterer.MarkerClusterer({ markers, map:this.map });
 
-      overlay.onRemove = function () {
-        d3.select(".accident").remove();
-      };
+    // let dataCount = data.length; // We should display a string saying how many crashes we are showing
+    // let overlay = this.overlay;
 
-      overlay.draw = function () {
-        let projection = this.getProjection(),
-          padding = 10;
+    // //Create the overlay that we will draw on
 
-        let circleScale = d3
-          .scaleLinear()
-          .domain([
-            d3.min(data, (d) => d.CRASH_SEVERITY_ID),
-            d3.max(data, (d) => d.CRASH_SEVERITY_ID),
-          ])
-          .range([5, 10])
-          .clamp(true);
+    // // Add the container when the overlay is added to the map.
+    // overlay.onAdd = function () {
+    //   let layer = d3
+    //     .select(this.getPanes().overlayMouseTarget)
+    //     .append("div")
+    //     .attr("class", "accident");
 
-        // Draw each marker as a separate SVG element.
-        // We could use a single SVG, but what size would it have?
-        let marker = layer.selectAll("svg").data(data);
+    //   overlay.onRemove = function () {
+    //     d3.select(".accident").remove();
+    //   };
 
-        let markerEnter = marker.enter().append("svg");
+    //   overlay.draw = function () {
+    //     let projection = this.getProjection(),
+    //       padding = 10;
 
-        // add the circle
-        markerEnter.append("circle");
+    //     let circleScale = d3
+    //       .scaleLinear()
+    //       .domain([
+    //         d3.min(data, (d) => d.CRASH_SEVERITY_ID),
+    //         d3.max(data, (d) => d.CRASH_SEVERITY_ID),
+    //       ])
+    //       .range([5, 10])
+    //       .clamp(true);
 
-        marker.exit().remove();
+    //     // Draw each marker as a separate SVG element.
+    //     // We could use a single SVG, but what size would it have?
+    //     let marker = layer.selectAll("svg").data(data);
 
-        marker = marker.merge(markerEnter);
+    //     let markerEnter = marker.enter().append("svg");
 
-        marker.each(transform).attr("class", "marker");
+    //     // add the circle
+    //     markerEnter.append("circle");
 
-        // style the circle
-        marker
-          .select("circle")
-          .attr("r", (d) => circleScale(d.CRASH_SEVERITY_ID))
-          .attr("cx", padding)
-          .attr("cy", padding)
-          .style("opacity", 0.8)
-          .attr("fill", "red")
-          .attr("id", (d) => "CRASH" + d.CRASH_ID)
-          .on("click", (event, data) => {
-            // console.log("clicked", data);
-            // console.log('clicledd', event);
-            d3.select("#details").html(
-              `<h3>Accident Details</h3>
-              <p>Accident ID: ${data.CRASH_ID}</p>
-              <p>Accident Date: ${data.CRASH_DATETIME}</p>
-              <p>Accident Severity: ${data.CRASH_SEVERITY_ID}</p>
-              <p>Road: ${data.MAIN_ROAD_NAME}</p>
-              <p>City/County: ${data.CITY}/${data.COUNTY_NAME}</p>
-              `
-            );
-          });
+    //     marker.exit().remove();
 
-        //transforms the markers to the right
-        // lat / lng using the projection from google maps
-        function transform(d) {
-          d = new google.maps.LatLng(+d.lat, +d.lon);
-          d = projection.fromLatLngToDivPixel(d);
-          return d3
-            .select(this)
-            .style("left", d.x - padding + "px")
-            .style("top", d.y - padding + "px");
-        }
-      };
-    };
-    // Bind our overlay to the map…
-    overlay.setMap(this.map);
+    //     marker = marker.merge(markerEnter);
+
+    //     marker.each(transform).attr("class", "marker");
+
+    //     // style the circle
+    //     marker
+    //       .select("circle")
+    //       .attr("r", (d) => circleScale(d.CRASH_SEVERITY_ID))
+    //       .attr("cx", padding)
+    //       .attr("cy", padding)
+    //       .style("opacity", 0.8)
+    //       .attr("fill", "red")
+    //       .attr("id", (d) => "CRASH" + d.CRASH_ID)
+    //       .on("click", (event, data) => {
+    //         // console.log("clicked", data);
+    //         // console.log('clicledd', event);
+    //         d3.select("#details").html(
+    //           `<h3>Accident Details</h3>
+    //           <p>Accident ID: ${data.CRASH_ID}</p>
+    //           <p>Accident Date: ${data.CRASH_DATETIME}</p>
+    //           <p>Accident Severity: ${data.CRASH_SEVERITY_ID}</p>
+    //           <p>Road: ${data.MAIN_ROAD_NAME}</p>
+    //           <p>City/County: ${data.CITY}/${data.COUNTY_NAME}</p>
+    //           `
+    //         );
+    //       });
+
+    //     //transforms the markers to the right
+    //     // lat / lng using the projection from google maps
+    //     function transform(d) {
+    //       d = new google.maps.LatLng(+d.lat, +d.lon);
+    //       d = projection.fromLatLngToDivPixel(d);
+    //       return d3
+    //         .select(this)
+    //         .style("left", d.x - padding + "px")
+    //         .style("top", d.y - padding + "px");
+    //     }
+    //   };
+    // };
+    // // Bind our overlay to the map…
+    // overlay.setMap(this.map);
   }
 
   colorCircles(circles, clicked) {
-    for (let i = 0; i < circles.length; i++) {
-      d3.select(`#CRASH${circles[i].CRASH_ID}`).classed("clicked", clicked);
-    }
+    // for (let i = 0; i < circles.length; i++) {
+    //   d3.select(`#CRASH${circles[i].CRASH_ID}`).classed("clicked", clicked);
+    // }
   }
 }
